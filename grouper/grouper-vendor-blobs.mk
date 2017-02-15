ded16705eb64116a77218c281d2dd34f159ee98c
Ziyan <jaraidaniel@gmail.com>
2017-02-15 21:48:53 +0000

    grouper: Reorganise structure

---
+++ grouper/grouper-vendor-blobs.mk
@@ -0,0 +1,4 @@
+VENDOR_FOLDER := vendor/nxp/grouper
+
+PRODUCT_COPY_FILES += \
+	$(VENDOR_FOLDER)/proprietary/vendor/firmware/libpn544_fw.so:system/vendor/firmware/libpn544_fw.so
---
+++ grouper/grouper-vendor.mk
@@ -0,0 +1,1 @@
+$(call inherit-product, vendor/nxp/grouper/grouper-vendor-blobs.mk)
